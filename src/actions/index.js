9d16bcfe5533e65c36d3352ec71f617592c08b04
Thomas (Noit) <tba@live.dk>
2021-03-17 14:52:10 +0100

    minor action change

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 // action creater stuff
 const fetchCV = () => async dispatch => {
     const respose = await cv-api.get("[OUR API URL HERE]");
-    dispatch({ type: "FETCH_DRIVERS", payload: respose.data.MRData.DriverTable.Drivers })
+    dispatch({ type: "FETCH_CV", payload: respose.data })
 };
