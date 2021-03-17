c16701feb341ea5414365314dddbe98ced42b1ed
Thomas (Noit) <tba@live.dk>
2021-03-17 13:03:32 +0100

    Added more index.js (action/reducer).

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import react from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
