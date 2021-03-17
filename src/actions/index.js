7e60119be04f1bf918a3372a1c7ae7f1092c1152
Thomas (Noit) <tba@live.dk>
2021-03-17 14:48:13 +0100

    Add: axios, action content

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
 // action creater stuff
+const fetchCV = () => async dispatch => {
+    const respose = await cv-api.get("[OUR API URL HERE]");
+    dispatch({ type: "FETCH_DRIVERS", payload: respose.data.MRData.DriverTable.Drivers })
+};
