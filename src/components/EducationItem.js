1963f124f7f7443aabed44ffb919d431adb50c83
Thomas (Noit) <tba@live.dk>
2021-03-17 15:05:46 +0100

    Hardcoded educationItem

---
+++ src/components/EducationItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { connect } from "react-redux";
+
+
+const educationItem = () => {
+    return (
+        <div class="row work-item">
+            <div class="col-md-3">
+                09/2019 -
+                Current
+        </div>
+            <div class="col-md-9">
+                <h3>Datamatiker </h3>
+                <h4>Zealand Academy, Roskilde</h4>
+                <ul>
+                    <li>Talking</li>
+                    <li>Chatting</li>
+                    <li>Conversing</li>
+                    <li>Monologing</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+const mapStateToProps = () => {
+
+}
+
+export default connect(mapStateToProps)(educationItem);
