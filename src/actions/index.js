bba0864ad9b574069c0712d2fe72273a5044d3a0
Thomas (Noit) <tba@live.dk>
2021-03-17 14:59:52 +0100

    added import of api in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 // action creater stuff
+
+import cvApi from "../apis/cv-api";
 const fetchCV = () => async dispatch => {
-    const respose = await cv-api.get("[OUR API URL HERE]");
+    const respose = await cvApi.get("[OUR API URL HERE]");
     dispatch({ type: "FETCH_CV", payload: respose.data })
 };
