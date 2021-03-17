0e0617a74284d7fab6105389b01373b065225c07
Thomas (Noit) <tba@live.dk>
2021-03-17 14:12:56 +0100

    App layout, SideBar w. comment layout.

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
+class App extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <section id="sidebar" className="col-md-4">
+                    
+                </section>
+            </div>
+        );
+    }
+}
---
+++ src/components/SideBar.js
@@ -0,0 +1,29 @@
+
+
+/* <div id="sidebar-header">
+        <h1>Thomas B. Andersen</h1>
+    </div>
+    <section>
+        <div class="section-header">
+            <h2>Contact</h2>
+        </div>
+        <div class="section-content">
+            <b>Address:</b> Hedeboparken 4 1. 213, 4000 Roskilde<br />
+            <b>Phone:</b> +45 60 90 20 17<br />
+            <b>E-mail:</b> tba@live.dk<br />
+        </div>
+    </section>
+    <section>
+        <div class="section-header">
+            <h2>Skills</h2>
+        </div>
+        <div class="section-content">
+            <ul>
+                <li>Skill 1</li>
+                <li>C#</li>
+                <li>Skill 2 with more text</li>
+                <li>Skill 3 with an overly long text</li>
+                <li>Skill 4 minimum</li>
+            </ul>
+        </div>
+    </section> */
