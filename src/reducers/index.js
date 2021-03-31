5c5290964cb05fc77f5b72ec58710f7d2f0ef2f1
Thomas (Noit) <tba@live.dk>
2021-03-31 09:41:28 +0200

    Removed comment.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,5 @@
 import { combineReducers } from "redux";
 
-
-// const educationReducer = (action) => {
-//     if(action.type == "FETCH_EDUCATIONS"){
-//         return action.payload;
-//     }
-// }
-
 const fetchEducationItems = (state = [], action) => {
     switch (action.type) {
         case 'FETCH_EDUCATION_ITEMS':
